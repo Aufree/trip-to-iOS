f55d5c58f0e719921e3aaf2b148a19ef7a1761ff
Aufree <freedomlijinfa@gmail.com>
2015-11-24 21:58:37 +0800

    Add fake lang

---
+++ fake_lang/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  fake_lang
+//
+//  Created by Aufree on 11/24/15.
+//  Copyright © 2015 Aufree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
